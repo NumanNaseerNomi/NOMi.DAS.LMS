b26d478c2addc36ee09ab190db0a0b7c4c08d68f
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2021-01-02 02:55:39 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+    <?php include 'assets\header.php'; ?>
+    <!-- [ Main Content ] start -->
+    <div class="container-fluid pt-5">
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+      <h1>Hello, world!</h1>
+    </div>
+    <!-- [ Main Content ] end -->
+    <?php include 'assets\footer.php'; ?>
