f7d02895a62a542638240775f111cf21c4e08d52
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2021-01-02 07:09:03 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,113 @@
-    <?php include 'assets\header.php'; ?>
-    <!-- [ Main Content ] start -->
-    <div class="container-fluid pt-5">
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
+<?php include 'assets\header.php'; ?>
+<div class="row">
+  <div class="col-md-6 col-xl-4">
+    <div class="card">
+      <div class="card-header">
+        <h5>Computer Science</h5>
+      </div>
+      <hr/ class="m-0">
+      <div class="card-block p-3">
+        <div class="row align-items-center justify-content-center">
+          
+          <div class="col-12">
+            <h4>Teacher Name</h4>
+            <p>Qualification</p>
+            <p>Institution</p>
+          </div>
+          <div class="col-12">
+            <h2 class="d-flex justify-content-center">4.5 <i class="fas fa-star f-12 m-l-10 text-c-yellow"></i></h2>
+          </div>
+          
+        </div>
+        <hr/>
+        <div class="d-flex justify-content-around">
+
+          <a class="" title="My Account" role="button"><h3><i class="fas fa-user"></i></h3></a>
+          <a class="" title="My Account" role="button"><h3><i class="fas fa-bullhorn"></i></h3></a>
+        </div>
+      </div>
     </div>
-    <!-- [ Main Content ] end -->
-    <?php include 'assets\footer.php'; ?>
+  </div>
+  <div class="col-md-6 col-xl-4">
+    <div class="card">
+      <div class="card-header">
+        <h5>Project Rating</h5>
+      </div>
+      <div class="card-block">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-6">
+            <h2 class="f-w-300 d-flex align-items-center float-left">4.3 <i class="fas fa-star f-12 m-l-10 text-c-yellow"></i></h2>
+          </div>
+          <div class="col-6">
+            <h6 class="f-w-300 d-flex  align-items-center float-right">0.4 <i class="fas fa-caret-up text-c-green f-24 m-l-10"></i></h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-6 col-xl-4">
+    <div class="card">
+      <div class="card-header">
+        <h5>Project Rating</h5>
+      </div>
+      <div class="card-block">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-6">
+            <h2 class="f-w-300 d-flex align-items-center float-left">4.3 <i class="fas fa-star f-12 m-l-10 text-c-yellow"></i></h2>
+          </div>
+          <div class="col-6">
+            <h6 class="f-w-300 d-flex  align-items-center float-right">0.4 <i class="fas fa-caret-up text-c-green f-24 m-l-10"></i></h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-6 col-xl-4">
+    <div class="card">
+      <div class="card-header">
+        <h5>Project Rating</h5>
+      </div>
+      <div class="card-block">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-6">
+            <h2 class="f-w-300 d-flex align-items-center float-left">4.3 <i class="fas fa-star f-12 m-l-10 text-c-yellow"></i></h2>
+          </div>
+          <div class="col-6">
+            <h6 class="f-w-300 d-flex  align-items-center float-right">0.4 <i class="fas fa-caret-up text-c-green f-24 m-l-10"></i></h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+<h1>Hello, world!</h1>
+
+<?php include 'assets\footer.php'; ?>
