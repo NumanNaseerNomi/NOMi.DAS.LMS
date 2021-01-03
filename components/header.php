e4ac1eec1462386cf33c3979b4afa3fd0df279f9
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2021-01-03 19:50:41 +0500

    Updated from  bootstrap-5.0.0-alpha to bootstrap-5.0.0-beta1
    Namespaced data attributes
    We’ve renamed all our data attributes to include bs as an infix, thereby namespacing all the HTML attributes that enable JavaScript behaviors from our plugins. See #31827 for details. It helps keep Bootstrap-required JavaScript triggers clearly identified throughout your projects.
    
    Making this change is a tad annoying, but easy enough to remedy with a find and replace. The new attributes work just like the old ones, just a little more specific. For example, here’s a dropdown button and menu with the newly renamed data-bs-toggle attribute.

--- components/header.php
+++ components/header.php
@@ -16,11 +16,11 @@
       <nav class="navbar p-0">
         <div class="container-fluid p-0">
           <div class="dropdown" id="preventDropdown">
-            <a class="nav-link dropdown-toggle navbar-brand" href="#" title="Menu" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-expanded="false"><i class="fas fa-bars"></i></a>
+            <a class="nav-link dropdown-toggle navbar-brand" href="#" title="Menu" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-bars"></i></a>
             <ul class="dropdown-menu menuDropdown" aria-labelledby="dropdownMenuLink">
               <li class="accordion" id="menuAccordion">
-                <a class="dropdown-item menuHeadingOne" type="button" data-toggle="collapse" data-target="#menuCollapseOne" aria-expanded="true" aria-controls="menuCollapseOne"><i class="fas fa-bell"></i>Collapsible #1</a>
-                <div id="menuCollapseOne" class="collapse" aria-labelledby="menuHeadingOne" data-parent="#menuAccordion">
+                <a class="dropdown-item menuHeadingOne" type="button" data-bs-toggle="collapse" data-bs-target="#menuCollapseOne" aria-expanded="true" aria-controls="menuCollapseOne"><i class="fas fa-bell"></i>Collapsible #1</a>
+                <div id="menuCollapseOne" class="collapse" aria-labelledby="menuHeadingOne" data-bs-parent="#menuAccordion">
                   <ul>
                     <li><a class="dropdown-item" href="#">Action</a></li>
                     <li><a class="dropdown-item" href="#">Action</a></li>
@@ -28,8 +28,8 @@
                     <li><a class="dropdown-item" href="#">Action</a></li>
                   </ul>
                 </div>
-                <a class="dropdown-item menuHeadingTwo" type="button" data-toggle="collapse" data-target="#menuCollapseTwo" aria-expanded="false" aria-controls="menuCollapseTwo"><i class="fas fa-user"></i>Collapsible #2</a>
-                <div id="menuCollapseTwo" class="collapse" aria-labelledby="menuHeadingTwo" data-parent="#menuAccordion">
+                <a class="dropdown-item menuHeadingTwo" type="button" data-bs-toggle="collapse" data-bs-target="#menuCollapseTwo" aria-expanded="false" aria-controls="menuCollapseTwo"><i class="fas fa-user"></i>Collapsible #2</a>
+                <div id="menuCollapseTwo" class="collapse" aria-labelledby="menuHeadingTwo" data-bs-parent="#menuAccordion">
                   <ul>
                     <li><a class="dropdown-item" href="#">Action</a></li>
                     <li><a class="dropdown-item" href="#">Action</a></li>
@@ -37,8 +37,8 @@
                     <li><a class="dropdown-item" href="#">Action</a></li>
                   </ul>
                 </div>
-                <a class="dropdown-item menuHeadingThree active" type="button" data-toggle="collapse" data-target="#menuCollapseThree" aria-expanded="false" aria-controls="menuCollapseThree"><i class="fas fa-tasks"></i>Collapsible #3</a>
-                <div id="menuCollapseThree" class="collapse" aria-labelledby="menuHeadingThree" data-parent="#menuAccordion">
+                <a class="dropdown-item menuHeadingThree active" type="button" data-bs-toggle="collapse" data-bs-target="#menuCollapseThree" aria-expanded="false" aria-controls="menuCollapseThree"><i class="fas fa-tasks"></i>Collapsible #3</a>
+                <div id="menuCollapseThree" class="collapse" aria-labelledby="menuHeadingThree" data-bs-parent="#menuAccordion">
                   <ul>
                     <li><a class="dropdown-item" href="#">Action</a></li>
                     <li><a class="dropdown-item" href="#">Action</a></li>
@@ -47,8 +47,8 @@
                   </ul>
                 </div>
                 <a class="dropdown-item" href="#"><i class="fas fa-language"></i>NOMi</a>
-                <a class="dropdown-item menuHeadingFour" type="button" data-toggle="collapse" data-target="#menuCollapseFour" aria-expanded="false" aria-controls="menuCollapseFour"><i class="fas fa-bell"></i>Collapsible #4</a>
-                <div id="menuCollapseFour" class="collapse" aria-labelledby="menuHeadingFour" data-parent="#menuAccordion">
+                <a class="dropdown-item menuHeadingFour" type="button" data-bs-toggle="collapse" data-bs-target="#menuCollapseFour" aria-expanded="false" aria-controls="menuCollapseFour"><i class="fas fa-bell"></i>Collapsible #4</a>
+                <div id="menuCollapseFour" class="collapse" aria-labelledby="menuHeadingFour" data-bs-parent="#menuAccordion">
                   <ul>
                     <li><a class="dropdown-item" href="#">Action</a></li>
                     <li><a class="dropdown-item" href="#">Action</a></li>
@@ -65,7 +65,7 @@
           </div>
           <div>
             <div class="btn-group">
-              <a class="dropdown-toggle navbar-brand" title="My TO-DOs" role="button" data-toggle="dropdown" aria-expanded="false"><i class="fas fa-tasks"></i></a>
+              <a class="dropdown-toggle navbar-brand" title="My TO-DOs" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-tasks"></i></a>
               <ul class="dropdown-menu dropdown-menu-right">
                 <li><a class="dropdown-item" href="#">Action</a></li>
                 <li><a class="dropdown-item" href="#">Another action</a></li>
@@ -75,7 +75,7 @@
               </ul>
             </div>
             <div class="btn-group">
-              <a class="dropdown-toggle navbar-brand" title="Notifications" role="button" data-toggle="dropdown" aria-expanded="false"><i class="fas fa-bell"></i></a>
+              <a class="dropdown-toggle navbar-brand" title="Notifications" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-bell"></i></a>
               <ul class="dropdown-menu dropdown-menu-right">
                 <li><a class="dropdown-item" href="#">Action</a></li>
                 <li><a class="dropdown-item" href="#">Another action</a></li>
@@ -85,7 +85,7 @@
               </ul>
             </div>
             <div class="btn-group">
-              <a class="dropdown-toggle navbar-brand" title="My Account" role="button" data-toggle="dropdown" aria-expanded="false"><i class="fas fa-user"></i></a>
+              <a class="dropdown-toggle navbar-brand" title="My Account" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user"></i></a>
               <ul class="dropdown-menu dropdown-menu-right">
                 <li class="iProfileHead">
                   <a class="dropdown-item text-center m-0 p-0 iProfileHead" href="#" title="My Profile">
