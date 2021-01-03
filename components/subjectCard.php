97935d9341b50d9a55bf30d25a0b8cd65a618c26
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2021-01-03 15:19:13 +0500

    updated

---
+++ components/subjectCard.php
@@ -0,0 +1,37 @@
+<!-- Subject Cards start-->
+<div class="row">
+  <!-- Subject Card start-->
+  <div class="col-md-6 col-xl-4">
+    <div class="card">
+      <div class="card-header">
+        <h5>Computer Science</h5>
+      </div>
+      <hr/ class="m-0">
+      <div class="card-block p-3">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-9">
+            <h4>Teacher / Class Name</h4>
+            <p class="m-0">Qualification / Section</p>
+            <p class="m-0">Institution / Session</p>
+          </div>
+          <div class="col-3">
+            <div class="d-flex">
+              <h3 class="m-0">99.9</h3>
+              <p class="m-0">%</p>
+            </div>
+            <p  class="m-0">Rating</p>
+          </div>
+        </div>
+        <hr/>
+        <div class="d-flex justify-content-around h3 m-0">
+          <a title="ClassWork" role="button" href=""><i class="fas fa-chalkboard-teacher"></i></a>
+          <a title="HomeWork" role="button" href=""><i class="fas fa-book-reader"></i></a>
+          <a title="Discussion Board" role="button" href=""><i class="fas fa-comments"></i></a>
+          <a title="Announcements" role="button" href=""><i class="fas fa-bullhorn"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Subject Card end-->
+</div>
+<!-- Subject Cards end-->
