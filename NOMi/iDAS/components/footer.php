06cba33842eef027290014e456d6423f094fce8e
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2021-01-05 02:41:03 +0500

    Separate Footer file creaded

---
+++ NOMi/iDAS/components/footer.php
@@ -0,0 +1,16 @@
+    <!-- Footer - Start -->
+    <footer>
+      <!-- Bootstrap JavaScript File - Start -->
+      <script type="text/javascript" src="assets\plugins\bootstrap\js\bootstrap.bundle.min.js"></script>
+      <!-- Bootstrap JavaScript File - Start -->
+      <script>
+      // Prevents menu from closing when clicked inside
+      document.getElementById("preventDropdown").addEventListener('click', function (event)
+      {
+      event.stopPropagation();
+      });
+      </script>
+    </footer>
+    <!-- Footer - End -->
+  </body>
+</html>
