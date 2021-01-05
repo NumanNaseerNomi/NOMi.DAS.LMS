87f0462b6522087e87e73268085fe2ef663e1661
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2021-01-05 20:17:45 +0500

    Update index.php

--- NOMi/iDAS/index.php
+++ NOMi/iDAS/index.php
@@ -11,49 +11,43 @@
     <link rel="stylesheet" type="text/css" href="assets\plugins\fontAwesome\css\fontAwesomeAll.min.css">
     <!-- Font Awesome CSS File - End -->
     <title>Hello, world!</title>
-
-<style>
-  .red { color: red; }
-  .bold { font-weight: bold; }
-  .nClasses {  }
-</style>
-
-<?php define('DangerText','red bold nClasses'); ?>
-
-
+    <?php define('DangerText','fs-1'); ?>
   </head>
   <body>
     <!-- Navigation Bar - Start -->
-    <header class="fixed-top p-0 fs-4    border-bottom shadow ">
+    <header class="fixed-top p-0 fs-4    border-bottom shadow-sm">
       <nav class="navbar p-0">
         <div class="container-fluid p-0">
-          <div class="dropdown" id="preventDropdown">
-            <a class="dropdown-toggle" title="Menu" role="button" id="menuDropdownButton" data-bs-toggle="dropdown" aria-expanded="false"><i class="far fa-bars"></i></a>
-            <div class="dropdown-menu dropdown-menu-start" aria-labelledby="menuDropdownButton">
-              <div><a class="dropdown-item" href="">Action</a></div>
-              <div><a class="dropdown-item" href="">Another action</a></div>
-              <hr class="dropdown-divider"/>
-              <div class="accordion" id="menuAccordion">
-                <a class="dropdown-item menuHeadingOne" type="button" data-bs-toggle="collapse" data-bs-target="#menuCollapseOne" aria-expanded="true" aria-controls="menuCollapseOne"><i class="fas fa-bell"></i>Collapsible #1</a>
-                <div id="menuCollapseOne" class="collapse" aria-labelledby="menuHeadingOne" data-bs-parent="#menuAccordion">
-                  <div><a class="dropdown-item active" href="">Action</a></div>
-                  <div><a class="dropdown-item" href="">Action</a></div>
-                </div>
-                <a class="dropdown-item menuHeadingTwo" type="button" data-bs-toggle="collapse" data-bs-target="#menuCollapseTwo" aria-expanded="false" aria-controls="menuCollapseTwo"><i class="fas fa-user"></i>Collapsible #2</a>
-                <div id="menuCollapseTwo" class="collapse" aria-labelledby="menuHeadingTwo" data-bs-parent="#menuAccordion">
-                  <div><a class="dropdown-item" href="">Action</a></div>
-                  <div><a class="dropdown-item" href="">Action</a></div>
+          <div>
+            <div class="dropdown ms-2" id="preventDropdown">
+              <a title="Menu" role="button" id="menuDropdownButton" data-bs-toggle="dropdown" aria-expanded="false"><i class="far fa-bars"></i></a>
+              <div class="dropdown-menu dropdown-menu-start" aria-labelledby="menuDropdownButton">
+                <div><a class="dropdown-item" href="">Action</a></div>
+                <div><a class="dropdown-item" href="">Another action</a></div>
+                <hr class="dropdown-divider"/>
+                <div class="accordion" id="menuAccordion">
+                  <a class="dropdown-item menuHeadingOne" type="button" data-bs-toggle="collapse" data-bs-target="#menuCollapseOne" aria-expanded="true" aria-controls="menuCollapseOne"><i class="fas fa-bell"></i>Collapsible #1</a>
+                  <div id="menuCollapseOne" class="collapse" aria-labelledby="menuHeadingOne" data-bs-parent="#menuAccordion">
+                    <div><a class="dropdown-item active" href="">Action</a></div>
+                    <div><a class="dropdown-item" href="">Action</a></div>
+                  </div>
+                  <a class="dropdown-item menuHeadingTwo" type="button" data-bs-toggle="collapse" data-bs-target="#menuCollapseTwo" aria-expanded="false" aria-controls="menuCollapseTwo"><i class="fas fa-user"></i>Collapsible #2</a>
+                  <div id="menuCollapseTwo" class="collapse" aria-labelledby="menuHeadingTwo" data-bs-parent="#menuAccordion">
+                    <a class="dropdown-item" href=""><i class="fas fa-user"></i> Action</a>
+                    <a class="dropdown-item" href=""><i class="fas fa-user"></i> Action</a>
+                    <a class="dropdown-item" href=""><i class="fas fa-language"></i> Action</a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
           <div>
-            <a href=""><b class="d-none d-sm-block">DAR-E-ARQAM</b><b class="d-block d-sm-none">DAS</b></a>
+            <b class="d-none d-sm-block">DAR-E-ARQAM</b><b class="d-block d-sm-none">DAS</b>
           </div>
           <div>
-            <div class="btn-group">
+            <div class="btn-group me-2">
               <div class="dropdown">
-                <a class="dropdown-toggle" title="My TO-DOs" role="button" id="myToDos" data-bs-toggle="dropdown" aria-expanded="false"><i class="far fa-tasks"></i></a>
+                <a title="My TO-DOs" role="button" id="myToDos" data-bs-toggle="dropdown" aria-expanded="false"><i class="far fa-tasks"></i></a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="myToDos">
                   <li><a class="dropdown-item" href="">Action</a></li>
                   <li><a class="dropdown-item" href="">Another action</a></li>
@@ -62,9 +56,9 @@
                 </ul>
               </div>
             </div>
-            <div class="btn-group">
+            <div class="btn-group me-2">
               <div class="dropdown">
-                <a class="dropdown-toggle" title="Notifications" role="button" id="Notifications" data-bs-toggle="dropdown" aria-expanded="false"><i class="far fa-bell"></i></a>
+                <a title="Notifications" role="button" id="Notifications" data-bs-toggle="dropdown" aria-expanded="false"><i class="far fa-bell"></i></a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="Notifications">
                   <li><a class="dropdown-item" href="">Action</a></li>
                   <li><a class="dropdown-item" href="">Another action</a></li>
@@ -83,15 +77,5 @@
       <h1>Heading H1</h1>
     </div>
     <!-- Main Container - End -->
-    <h1>Hello, world!</h1>
-
-
-    <p class="fs-1">.fs-1 text</p>
-<p class="fs-2">.fs-2 text</p>
-<p class="fs-3">.fs-3 text</p>
-<p class="fs-4">.fs-4 text</p>
-<p class="fs-5">.fs-5 text</p>
-<p class="fs-6">.fs-6 text</p>
-
-<span class="<?php echo DangerText ?>"> Le Champion est Ici </span>
+    <span class="<?php echo DangerText ?>"> Le Champion est Ici </span>
     <?php require 'components\footer.php'; ?>
