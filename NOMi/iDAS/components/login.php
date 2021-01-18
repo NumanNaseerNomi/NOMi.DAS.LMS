82ca9c922417ce7966c678721ac1899b62e2f5bb
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2021-01-18 19:39:11 +0500

    Create login.php

---
+++ NOMi/iDAS/components/login.php
@@ -0,0 +1,22 @@
+<div  class="d-flex justify-content-center">
+	<form class="card mb-3" style="width: 16rem">
+		<div class="card-body">
+			<div class="text-center">
+				<i class="far fa-user-unlock fa-2x mb-3"></i>
+				<h3 class="card-title mb-3">Login</h3>
+			</div>
+			<div class="mb-3">
+				<label for="userName" class="form-label">User Name</label>
+				<input type="text" class="form-control form-control-sm bgDarkBlack border-dark fcLightBlack" id="userName" placeholder="numan.naseer.nomi" required>
+			</div>
+			<div class="mb-3">
+				<label for="password" class="form-label">Password</label>
+				<input type="password" class="form-control form-control-sm bgDarkBlack border-dark fcLightBlack" id="password" placeholder="NumanNaseerNomi" required>
+			</div>
+			<div class="d-grid gap-2">
+				<button type="submit" class="btn btn-outline-primary btn-sm mb-3">Login</button>
+			</div>
+			<p class="text-muted m-0">Forgot password? <a href="">Reset</a></p>
+		</div>
+	</form>
+</div>
