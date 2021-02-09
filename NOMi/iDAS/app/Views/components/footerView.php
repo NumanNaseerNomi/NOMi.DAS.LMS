81fd2012563627dadb36c215c3323646ffe2d889
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2021-02-09 05:26:32 +0500

    Update footerView.php

--- NOMi/iDAS/app/Views/components/footerView.php
+++ NOMi/iDAS/app/Views/components/footerView.php
@@ -11,6 +11,59 @@
         event.stopPropagation();
       });
       </script>
+
+
+<!--
+<script>
+  function sessionActivityWatcher()
+  {
+    var maxInactivityTime = 0.10; //In minutes.
+
+    
+    var isExpired = false;
+    maxInactivityTime *= (60*1000); //1000 milliseconds = 1 second.
+
+    var timeInterval = setInterval(sessionExpiredAlert, maxInactivityTime);
+
+    function sessionExpiredAlert()
+    {
+      clearInterval(timeInterval);
+      
+      isExpired = true;
+      var sessionTimeOutModal = new bootstrap.Modal(document.getElementById('sessionTimeOutModal'),
+      {
+        keyboard: false,
+        focus:    true,
+        backdrop: "static"
+      });
+
+      sessionTimeOutModal.show();
+      console.log('updated');
+    }
+    //Reset time on any activity
+    function resetActivityTime()
+    {
+      if (!isExpired)
+      {
+        clearInterval(timeInterval);
+        timeInterval = setInterval(sessionExpiredAlert, maxInactivityTime);
+        console.log('updated');
+      } 
+    }
+
+    var activityEvents = ['mousedown', 'mousemove', 'keydown', 'scroll', 'touchstart']; //An array of DOM events.
+    //register the activity function as the listener parameter.
+    for (var i = 0; i < activityEvents.length; i++)
+    {
+      document.addEventListener(activityEvents[i], resetActivityTime, true);
+    }
+  }
+
+  <?php if (isLoggedIn()) { ?>
+    sessionActivityWatcher();
+    console.log('logged in');
+  <?php } ?>
+</script> -->
     </footer>
     <!-- NOMi - Footer - End -->
   </body>
