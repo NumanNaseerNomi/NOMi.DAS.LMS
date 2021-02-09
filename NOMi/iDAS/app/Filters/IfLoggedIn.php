fe6c5df0093ff605d41dc136631878269d2d5d35
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2021-02-09 05:26:03 +0500

    Update IfLoggedIn.php

--- NOMi/iDAS/app/Filters/IfLoggedIn.php
+++ NOMi/iDAS/app/Filters/IfLoggedIn.php
@@ -10,7 +10,7 @@ class IfLoggedIn implements FilterInterface
 	public function before(RequestInterface $request, $arguments = null)
 	{
 		$session = session();
-		if ($session->has('iDASUser'))
+		if ($session->has('iDASUser') && $session->get('iDASUser')->isLoggedIn)
 		{
 			return true;
 		}
