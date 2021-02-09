42bb6508edc89530ccb244ea89ad1170e4281767
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2021-02-09 05:25:45 +0500

    Create UserController.php

---
+++ NOMi/iDAS/app/Controllers/UserController.php
@@ -0,0 +1,63 @@
+<?php namespace App\Controllers;
+
+use App\Models\UserModel;
+
+class UserController extends BaseController
+{
+	public function index()
+	{
+		echo view('components/headerView');
+
+		echo view('components/footerView');
+	}
+
+	public function login()
+	{
+		if ($this->request->getMethod() == 'post')
+		{
+			if (! $this->validate(
+				[
+					'userName' => ['label' => 'User Name', 'rules' => 'required'],
+    				'userPassword' => ['label' => 'Password', 'rules' => 'required|min_length[6]|differs[userName]'],
+				]))
+	        {
+	        	$validationErrors = (Object)$this->validator->getErrors();
+	        	return redirect()->back();
+	        }
+	        else
+	        {
+	        	$userModel = new UserModel();
+				$user = $userModel->login((object)$this->request->getPost());
+
+				if ($user)
+				{
+					$sessionData =
+					[
+					    'userID'	=> $user->id,
+					    'userName'	=> $user->userName,
+					    'isLoggedIn'=> true,
+					];
+
+					$this->session->set('iDASUser', (object)$sessionData);
+					$this->session->setFlashData('success', 'Logged In Successfully..!');
+					return redirect()->to('/');
+				}
+				else
+				{
+					$this->session->setFlashData('danger', 'Incorrect User Name or Password..!');
+					return redirect()->back();
+				}
+	        }
+		}
+		echo view('components/headerView');
+		echo view('loginView');
+		echo view('components/footerView');
+	}
+
+	public function logout()
+	{
+		$this->session->remove('iDASUser');
+		$this->session->setFlashData('success', 'Logged Out Successfully..!');
+		return redirect()->to('login');
+	}
+}
