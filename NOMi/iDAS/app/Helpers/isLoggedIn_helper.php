15adf353f751cd4a6cd6b0decbdc5c3fe7ea8cfb
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2021-02-09 05:26:17 +0500

    Update isLoggedIn_helper.php

--- NOMi/iDAS/app/Helpers/isLoggedIn_helper.php
+++ NOMi/iDAS/app/Helpers/isLoggedIn_helper.php
@@ -5,7 +5,7 @@ use Config\Services;
 function isLoggedIn()
 {
 	$session = session();
-	if ($session->has('iDASUser'))
+	if ($session->has('iDASUser') && $session->get('iDASUser')->isLoggedIn)
 	{
 		return true;
 	}
