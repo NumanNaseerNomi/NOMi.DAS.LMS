db34492de14eeefb4ab01b51639f864c9cd90f7f
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2021-04-22 21:08:36 +0500

    Update subjectCardView.php

--- NOMi/iDAS/app/Views/components/subjectCardView.php
+++ NOMi/iDAS/app/Views/components/subjectCardView.php
@@ -1,6 +1,6 @@
-<!-- Subject Cards - Start-->
+<!-- NOMi - Subject Cards - Start-->
 <div class="row">
-  <!-- Subject Card - Start-->
+  <!-- NOMi - Subject Card - Start-->
   <div class="col-md-6 col-xl-4 p-3">
     <div class="card">
       <h4 class="m-3 mb-0">Computer Science</h4>
@@ -22,14 +22,14 @@
         </div>
         <hr/>
         <div class="d-flex justify-content-around h3 m-0">
-          <a title="ClassWork" role="button" href=""><i class="far fa-chalkboard-teacher"></i></a>
-          <a title="HomeWork" role="button" href=""><i class="far fa-book-reader"></i></a>
-          <a title="Discussion Board" role="button" href=""><i class="far fa-comments"></i></a>
-          <a title="Announcements" role="button" href=""><i class="far fa-bullhorn"></i></a>
+          <a title="ClassWork" role="button" href="<?php echo base_url('ClassWork'); ?>"><i class="far fa-chalkboard-teacher"></i></a>
+          <a title="HomeWork" role="button" href="<?php echo base_url('HomeWork'); ?>"><i class="far fa-book-reader"></i></a>
+          <a title="Discussion Board" role="button" href="<?php echo base_url('DiscussionBoard'); ?>"><i class="far fa-comments"></i></a>
+          <a title="Announcements" role="button" href="<?php echo base_url('Announcements'); ?>"><i class="far fa-bullhorn"></i></a>
         </div>
       </div>
     </div>
   </div>
-  <!-- Subject Card - End-->
+  <!-- NOMi - Subject Card - End-->
 </div>
-<!-- Subject Cards - Start-->
+<!-- NOMi - Subject Cards - Start-->
