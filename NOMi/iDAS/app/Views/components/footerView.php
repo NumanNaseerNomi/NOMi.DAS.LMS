814be6f6f86b834574eaa40c7991366243e4ca82
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2021-04-22 21:07:12 +0500

    Update footerView.php

--- NOMi/iDAS/app/Views/components/footerView.php
+++ NOMi/iDAS/app/Views/components/footerView.php
@@ -3,7 +3,7 @@
     <footer>
       <!-- NOMi - Bootstrap JavaScript File - Start -->
       <script type="text/javascript" src="<?php echo base_url('/assets/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
-      <!-- NOMi - Bootstrap JavaScript File - Start -->
+      <!-- NOMi - Bootstrap JavaScript File - End -->
       <script>
       // Prevents menu from closing when clicked inside
       document.getElementById("preventDropdown").addEventListener('click', function (event)
