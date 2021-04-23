e1e84c75bc6385d4a853ffb57be7a155708617f5
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2021-04-23 23:41:53 +0500

    Update UsersModel.php

--- NOMi/iDAS/app/Models/UsersModel.php
+++ NOMi/iDAS/app/Models/UsersModel.php
@@ -21,4 +21,74 @@ class UsersModel extends Model
     		return false;
     	}
 	}
+
+    public function getUser($userID)
+    {
+        $user = $this->getWhere(['id' => $userID])->getRow();
+
+        if ($user)
+        {
+            return $user;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public function getUsers()
+    {
+        $users = $this->get()->getResult();
+
+        if ($users)
+        {
+            return $users;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+	public function getUserName($userID)
+	{
+		$user = $this->getWhere(['id' => $userID])->getRow();
+
+		if ($user)
+    	{
+    		return $user->userName;
+    	}
+    	else
+    	{
+    		return false;
+    	}
+	}
+
+	public function getUserRoleID($userID)
+	{
+		$user = $this->getWhere(['id' => $userID])->getRow();
+
+		if ($user)
+    	{
+    		return $user->userRoleID;
+    	}
+    	else
+    	{
+    		return false;
+    	}
+	}
+
+	public function getUserCandidateID($userID)
+	{
+		$user = $this->getWhere(['id' => $userID])->getRow();
+
+		if ($user)
+    	{
+    		return $user->userCandidateID;
+    	}
+    	else
+    	{
+    		return false;
+    	}
+	}
 }
