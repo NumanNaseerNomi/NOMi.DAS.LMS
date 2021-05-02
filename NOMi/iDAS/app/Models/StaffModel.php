ff774cf79cbd24b5eac7c7312a4d0aebf08ba2e6
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2021-05-02 13:31:20 +0500

    Update StaffModel.php

--- NOMi/iDAS/app/Models/StaffModel.php
+++ NOMi/iDAS/app/Models/StaffModel.php
@@ -6,11 +6,25 @@ class StaffModel extends Model
 {
 	protected $table			= 'staff';
 	protected $returnType		= 'object';
-	protected $allowedFields	= ['name'];
+	// protected $allowedFields	= ['name'];
 
-    public function getStaff($staffID)
+    public function getStaffById($id)
     {
-        $staff = $this->getWhere(['id' => $staffID])->getRow();
+        $staffById = $this->getWhere(['id' => $id])->getRow();
+
+        if ($staffById)
+        {
+            return $staffById;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public function getStaff()
+    {
+        $staff = $this->get()->getResult();
 
         if ($staff)
         {
@@ -22,59 +36,45 @@ class StaffModel extends Model
         }
     }
 
-    public function getAllStaff()
-    {
-        $allStaff = $this->get()->getResult();
+    // public function getStaffNID($staffID)
+    // {
+    //     $staff = $this->getWhere(['id' => $staffID])->getRow();
 
-        if ($allStaff)
-        {
-            return $allStaff;
-        }
-        else
-        {
-            return false;
-        }
-    }
+    //     if ($staff)
+    //     {
+    //         return $staff->nid;
+    //     }
+    //     else
+    //     {
+    //         return false;
+    //     }
+    // }
 
-    public function getStaffNID($staffID)
-    {
-        $staff = $this->getWhere(['id' => $staffID])->getRow();
+    // public function getStaffName($staffID)
+    // {
+    //     $staff = $this->getWhere(['id' => $staffID])->getRow();
 
-        if ($staff)
-        {
-            return $staff->nid;
-        }
-        else
-        {
-            return false;
-        }
-    }
+    //     if ($staff)
+    //     {
+    //         return $staff->name;
+    //     }
+    //     else
+    //     {
+    //         return false;
+    //     }
+    // }
 
-    public function getStaffName($staffID)
-    {
-        $staff = $this->getWhere(['id' => $staffID])->getRow();
+    // public function getStaffDOB($staffID)
+    // {
+    //     $staff = $this->getWhere(['id' => $staffID])->getRow();
 
-        if ($staff)
-        {
-            return $staff->name;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    public function getStaffDOB($staffID)
-    {
-        $staff = $this->getWhere(['id' => $staffID])->getRow();
-
-        if ($staff)
-        {
-            return $staff->dob;
-        }
-        else
-        {
-            return false;
-        }
-    }
+    //     if ($staff)
+    //     {
+    //         return $staff->dob;
+    //     }
+    //     else
+    //     {
+    //         return false;
+    //     }
+    // }
 }
