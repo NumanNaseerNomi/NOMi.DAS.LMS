691fb9f68d8690d139807246ddd7d95397a9a780
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2021-05-29 13:51:31 +0500

    old files deleted

--- NOMi/iDAS/das/index.php
+++
@@ -1,5 +0,0 @@
-<?php require 'components\header.php'; ?> <hr>
-<?php require 'components\subjectCard.php'; ?> <hr>
-<?php require 'components\admissionForm.php'; ?> <hr>
-<?php require 'components\login.php'; ?> <hr>
-<?php require 'components\footer.php'; ?> <hr>
