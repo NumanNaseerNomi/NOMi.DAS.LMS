b3a451b96547b5d9683e68c61242f8b34fa301ff
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2021-06-07 21:32:13 +0500

    Update Routes.php

--- NOMi/iDAS/app/Config/Routes.php
+++ NOMi/iDAS/app/Config/Routes.php
@@ -35,7 +35,8 @@ $routes->match(['get', 'post'],'Login', 'UserController::login', ['filter' => 'I
 
 
 /* NOMi - Side Navigation Bar Routes - Start */
-$routes->get('/', 'HomeController::index');
+// $routes->get('/', 'HomeController::index');
+$routes->get('/', 'HomeController::tempComingSoon');
 $routes->get('GradeBook', 'GradeBookController::index');
 $routes->get('AccountBook', 'HomeController::tempComingSoon');
 $routes->get('TimeTable', 'HomeController::tempComingSoon');
