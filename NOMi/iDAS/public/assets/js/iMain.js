78b3e709d4d3c8e5b357a886b6f3551c89402086
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2021-10-14 19:40:05 +0500

    Update iMain.js

--- NOMi/iDAS/public/assets/js/iMain.js
+++ NOMi/iDAS/public/assets/js/iMain.js
@@ -56,25 +56,10 @@ function plotChart(chartConfig)
 // NOMi - Plot Chart with Chart.JS Plugin - End
 
 // NOMi - PWA Support - Start
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', function() {
-//     navigator.serviceWorker.register('swiDAS.js').then(function(registration) {
-//       // Registration was successful
-//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//     }, function(err) {
-//       // registration failed :(
-//       console.log('ServiceWorker registration failed: ', err);
-//     });
-//   });
-// }
-
-
-
 if('serviceWorker' in navigator)
 {
   navigator.serviceWorker.register('iDASServiceWorker.js')
     .then(reg => console.log('ServiceWorker registration successful with scope: ', reg.scope))
     .catch(err => console.log('ServiceWorker registration failed: ', err));
 }
-
 // NOMi - PWA Support - End
